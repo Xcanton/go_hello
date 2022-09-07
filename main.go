d5b0c0144dd050664082dcf1b4e76332022d5bd2
xupeixin <2352301553@qq.com>
2022-09-07 14:59:47 +0800

    struct definition

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 
-func main(){
-	// go中数组地址代表整一个数组，并非c中头节点指针，所以赋值传递数组时通常只使用指针，减小开销
-	var a = [...]int{1,2,3}
-	var b = &a
+type struct_variable_type struct {
+	a int
+	b int
+	c string
+}
 
-	for i,v := range b { fmt.Println(i,v) }
+func main(){
+	test := struct_variable_type {1,1,"set"}
+	fmt.Println(test.a)
 }
